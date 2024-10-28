0eef6dcd527bd542899de2719af6a6535537f235
Natalie Wong <natnatwong531@gmail.com>
2024-10-28 17:22:21 -0400

    Update more_variables.py

--- more_variables.py
+++ more_variables.py
@@ -1,1 +1,9 @@
+#We have to buy 4 apples, $0.5 per one apple
+item = "Apples"
+price = 0.5
+quantity = 4
 
+#calculate the total price
+total = price * quantity
+
+print("The total price of {quantity} 4 apple was ${total}.")
