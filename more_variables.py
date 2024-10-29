91d604ac4188815a0395f58307d0324d28420d1e
Natalie Wong <natnatwong531@gmail.com>
2024-10-29 12:35:05 -0400

    Update more_variables.py

--- more_variables.py
+++ more_variables.py
@@ -6,10 +6,6 @@ quantity = 4
 #calculate the total price
 total = price * quantity
 
-print("The total price of {quantity} 4 apple was ${total}.")
+#f-string
+print(f"The total price of {quantity} apples was ${total}.")
 
-
-#Without knowing any of the values stored in the variables, how do you know the line games_remaining = total_season_games - games_played must be correct?
-#total_season_games = the total number of games
-#games_played = the number of games already played
-#To find the number of games remaining you have to subtract the number of games played from the total
