78ce3b69026483c7ac26aa319c2335238aaaab47
Natalie Wong <natnatwong531@gmail.com>
2024-10-29 12:46:48 -0400

    Update using_variables.py

--- using_variables.py
+++ using_variables.py
@@ -1,4 +1,27 @@
-#numbers of apple
-items = 4
+#f-strings
 
-print(f"I have {items} apple")
+# Define variables
+age = 25
+height = 175
+name = "Petter"
+
+# Output using f-strings
+print(f"My name is {name}, I am {age} years old and I am {height} cm tall. ")
+
+
+#int 
+
+age = 25
+height = 175
+name = "Petter"
+
+print("My name is {}, I'm {} years old and I am {height} cm tall. ".format(name, age, height))
+
+
+#str
+
+age = 25
+height = 1.75
+name = "John"
+
+print("My name is " + str(name) + ", I am " + str(age) + " years old and I am" + str(height) + " cm tall. ")
