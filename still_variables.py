f1ad990e839420c0e3d541cc4adb22051df693d1
Natalie Wong <natnatwong531@gmail.com>
2024-10-29 12:50:08 -0400

    Update still_variables.py

--- still_variables.py
+++ still_variables.py
@@ -2,4 +2,4 @@ name = Natalie
 graduate = 2027
 
 # Output using f-strings
-print(f"My name is "name" and I'll graduate in "graduate". ")
+print(f"My name is {name} and I'll graduate in {graduate}. ")
